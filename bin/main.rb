2bebf85264cf4d460dad6547e83b2936a49d2681
Samitti <samuelteweldebrhan@gmail.com>
2020-04-28 17:25:36 +0300

    add user input of symbol check

--- bin/main.rb
+++ bin/main.rb
@@ -13,4 +13,22 @@ player_2 = gets.chomp.capitalize
 
 puts "The Player 2 is #{player_2}"
 
+puts "Player 1 Please Select Your Symbol X or O"
+
+player_1_sym = gets.chomp.upcase
+
+until player_1_sym == "X" || player_1_sym == "O"
+  puts "Please choose the correct symbol X or O"
+  player_1_sym = gets.chomp.upcase
+end
+
+if player_1_sym == "X"
+  player_2_sym = "O" 
+else
+  player_2_sym = "X"
+end
+
+puts "#{player_1} is #{player_1_sym} and #{player_2} is #{player_2_sym}"
+
+
 
