e7617b247d345d630e833ec93f0ef0b23e4c7ffb
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-04-28 19:55:37 +0600

    Add prompts to the game

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World'
+puts "-------------Welcome to Tic Tak Toe-----------------"
+print "Enter Player 1 name: "
+
+player_1 = gets.chomp.capitalize
+
+puts "The Player 1 is #{player_1}"
+
+print "Enter Player 2 name: "
+
+player_2 = gets.chomp.capitalize
+
+puts "The Player 2 is #{player_2}"
+
+
