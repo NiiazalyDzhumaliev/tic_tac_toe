6d6d4eb13d263a6372fdbe096c162bf27037129a
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-04-30 19:32:39 +0600

    Add board class

--- bin/main.rb
+++ bin/main.rb
@@ -1,34 +1,31 @@
 #!/usr/bin/env ruby
 
-puts "-------------Welcome to Tic Tak Toe-----------------"
-print "Enter Player 1 name: "
+puts '-------------Welcome to Tic Tak Toe-----------------'
+print 'Enter Player 1 name: '
 
 player_1 = gets.chomp.capitalize
 
 puts "The Player 1 is #{player_1}"
 
-print "Enter Player 2 name: "
+print 'Enter Player 2 name: '
 
 player_2 = gets.chomp.capitalize
 
 puts "The Player 2 is #{player_2}"
 
-puts "Player 1 Please Select Your Symbol X or O"
+puts 'Player 1 Please Select Your Symbol X or O'
 
 player_1_sym = gets.chomp.upcase
 
-until player_1_sym == "X" || player_1_sym == "O"
-  puts "Please choose the correct symbol X or O"
+until player_1_sym == 'X' || player_1_sym == 'O'
+  puts 'Please choose the correct symbol X or O'
   player_1_sym = gets.chomp.upcase
 end
 
-if player_1_sym == "X"
-  player_2_sym = "O" 
-else
-  player_2_sym = "X"
-end
+player_2_sym = if player_1_sym == 'X'
+                 'O'
+               else
+                 'X'
+               end
 
 puts "#{player_1} is #{player_1_sym} and #{player_2} is #{player_2_sym}"
-
-
-
---
+++ lib/board.rb
@@ -0,0 +1,39 @@
+class Board
+  def initialize
+    @board = [
+      %w[1 2 3],
+      %w[4 5 6],
+      %w[7 8 9]
+    ]
+  end
+
+  def display_board
+    i = 0
+    while i < 3
+      x = 0
+      while x < 3
+        print @board[i][x]
+        print '|'
+        x += 1
+      end
+      i += 1
+      puts ''
+    end
+  end
+
+  def update_board(player_sym, move)
+    i = 0
+    while i < 3
+      x = 0
+      while x < 3
+        if @board[i][x] == move.to_s
+          @board[i][x] = player_sym
+          return true
+        end
+        x += 1
+      end
+      i += 1
+    end
+    false
+  end
+end
