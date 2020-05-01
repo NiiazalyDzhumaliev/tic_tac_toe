7029b43b63d8fc5c223406186775595bdc6065d5
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-01 15:04:23 +0600

    Update main.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,31 +1,82 @@
 #!/usr/bin/env ruby
+require_relative '../lib/game_logic.rb'
 
 puts '-------------Welcome to Tic Tak Toe-----------------'
 print 'Enter Player 1 name: '
 
-player_1 = gets.chomp.capitalize
+player_one = gets.chomp.capitalize
 
-puts "The Player 1 is #{player_1}"
+puts "The Player 1 is #{player_one}"
 
 print 'Enter Player 2 name: '
 
-player_2 = gets.chomp.capitalize
+player_two = gets.chomp.capitalize
 
-puts "The Player 2 is #{player_2}"
+puts "The Player 2 is #{player_two}"
 
 puts 'Player 1 Please Select Your Symbol X or O'
 
-player_1_sym = gets.chomp.upcase
+player_one_sym = gets.chomp.upcase
 
-until player_1_sym == 'X' || player_1_sym == 'O'
+while (player_one_sym != 'X') && (player_one_sym != 'O')
   puts 'Please choose the correct symbol X or O'
-  player_1_sym = gets.chomp.upcase
+  player_one_sym = gets.chomp.upcase
 end
 
-player_2_sym = if player_1_sym == 'X'
-                 'O'
-               else
-                 'X'
-               end
+player_two_sym = if player_one_sym == 'X'
+                   'O'
+                 else
+                   'X'
+                 end
 
-puts "#{player_1} is #{player_1_sym} and #{player_2} is #{player_2_sym}"
+puts "#{player_one} is #{player_one_sym} and #{player_two} is #{player_two_sym}"
+
+current_player = nil
+
+def move_check(move)
+  if (1..9).any?(move)
+    true
+  else
+    false
+  end
+end
+
+def update_board(_move)
+  puts 'Validate move in the Board'
+  puts 'Update Board'
+  puts 'Displays Board'
+end
+
+def check_winner(_player)
+  puts 'check for win condition'
+  puts 'If winner Game Over else Continue Game'
+end
+
+game_on = true
+count = 0
+while game_on
+  game_on = false if count == 8
+  break unless game_on
+
+  if current_player == player_one
+    current_player = player_two
+    puts "#{current_player} choose from available moves: "
+    player_move = gets.chomp.to_i
+    until move_check(player_move)
+      puts " #{current_player} choose a correct move: "
+      player_move = gets.chomp.to_i
+    end
+  else
+    current_player = player_one
+    puts "#{current_player} select from available moves: "
+    player_move = gets.chomp.to_i
+    until move_check(player_move)
+      puts " #{current_player} make a correct move: "
+      player_move = gets.chomp.to_i
+    end
+  end
+  update_board(player_move)
+  check_winner(current_player)
+  count += 1
+end
+puts 'Game Over / Draw move ' unless game_on
