3900b6723cd167267491f24489faa8d14a73620e
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-05 18:39:28 +0600

    Move the win_array to Board class

--- lib/board.rb
+++ lib/board.rb
@@ -4,7 +4,20 @@ class Board
       %w[1 2 3],
       %w[4 5 6],
       %w[7 8 9]
-    ]    
+    ]
+  end
+
+  def win_generator
+    @win_arr = [
+      [@board[0][0], @board[0][1], @board[0][2]],
+      [@board[1][0], @board[1][1], @board[1][2]],
+      [@board[2][0], @board[2][1], @board[2][2]],
+      [@board[0][0], @board[1][0], @board[2][0]],
+      [@board[0][1], @board[1][1], @board[2][1]],
+      [@board[0][2], @board[1][2], @board[2][2]],
+      [@board[0][0], @board[1][1], @board[2][2]],
+      [@board[0][2], @board[1][1], @board[2][0]]
+    ]
   end
 
   def display_board
--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,18 +1,8 @@
 require_relative '../lib/board.rb'
 
-class GameLogic < Board 
-
+class GameLogic < Board
   def check_winner(current_sym)
-    @win_arr = [
-      [@board[0][0], @board[0][1], @board[0][2]],
-      [@board[1][0], @board[1][1], @board[1][2]],
-      [@board[2][0], @board[2][1], @board[2][2]],
-      [@board[0][0], @board[1][0], @board[2][0]],
-      [@board[0][1], @board[1][1], @board[2][1]],
-      [@board[0][2], @board[1][2], @board[2][2]],
-      [@board[0][0], @board[1][1], @board[2][2]],
-      [@board[0][2], @board[1][1], @board[2][0]]
-    ]
+    win_generator
     @win_arr.each do |sub_array|
       return true if sub_array.all?(current_sym)
     end
