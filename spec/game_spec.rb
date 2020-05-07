069c35932f3b2d505a81239d9e2dac4318333860
Samitti <samuelteweldebrhan@gmail.com>
2020-05-07 13:13:41 +0300

    add Board class test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -26,34 +26,49 @@ describe GameStart do
 end
 
 describe Player do
-  let(:player){Player.new('John', 'X', 'O')}
+  let(:player) { Player.new('John', 'X', 'O') }
 
-  describe "#sym_check" do
+  describe '#sym_check' do
     it "returns next player's symbol" do
-      expect(player.sym_check("John")).to eql('O')
-    end    
+      expect(player.sym_check('John')).to eql('O')
+    end
   end
 
-  describe "#move_check" do
-    it "returns false if move other than (1-9)" do
+  describe '#move_check' do
+    it 'returns false if move other than (1-9)' do
       expect(player.move_check(11)).to eql(false)
     end
   end
 end
 
 describe GameLogic do
-  let(:game_logic){GameLogic.new}
+  let(:game_logic) { GameLogic.new }
 
-  describe "#check_winner" do
-    it "returns false if no one wins with passed symbol" do
+  describe '#check_winner' do
+    it 'returns false if no one wins with passed symbol' do
       expect(game_logic.check_winner('X')).to eql(false)
     end
   end
 
-  describe "#draw_check" do
-    it "returs true while there is available move" do
+  describe '#draw_check' do
+    it 'returs true while there is available move' do
       expect(game_logic.draw_check).to eql(true)
     end
   end
 end
-  
+
+describe Board do
+  let(:board) { Board.new }
+
+  describe '#display_board' do
+    it 'returns the board' do
+      expect(board.display_board).to eql("1|2|3|\n4|5|6|\n7|8|9|\n")
+    end
+  end
+
+  describe '#update_board' do
+    it 'returns true if the board is updated' do
+      expect(board.update_board('X', 1)).to eql(true)
+    end
+  end
+end
