6427a6cae92d3626e28eff6e5b36b3d4e642e427
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-07 14:34:10 +0600

    Add name_check test

---
+++ spec/game_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/game_logic.rb'
+require_relative '../lib/player.rb'
+require_relative '../lib/board.rb'
+require_relative '../lib/game_start.rb'
+
+describe GameStart do
+  let(:game_start){GameStart.new}
+
+  context "#name_check" do
+    it "it returns false if empty" do
+      expect(game_start.name_check('')).to eq(false)
+    end
+  end
+end
