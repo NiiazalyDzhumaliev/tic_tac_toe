f70b98bc8f25aa2ec35be7e1b28a7987b45d9fc8
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-05-07 15:13:09 +0600

    Add player class' tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -24,3 +24,19 @@ describe GameStart do
     end
   end
 end
+
+describe Player do
+  let(:player){Player.new('John', 'X', 'O')}
+
+  describe "#sym_check" do
+    it "returns next player's symbol" do
+      expect(player.sym_check("John")).to eql('O')
+    end    
+  end
+
+  describe "#move_check" do
+    it "returns false if move other than (1-9)" do
+      expect(player.move_check(11)).to eql(false)
+    end
+  end
+end
